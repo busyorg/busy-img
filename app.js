814855ffd80fe151696e3a249dbf0f431bad4617
Nilesh Suthar <nileshsuthar@live.in>
2016-08-30 12:08:00 +0530

    Remove cloudinary config

--- app.js
+++ app.js
@@ -51,11 +51,4 @@ app.use(function(err, req, res, next) {
   }]);
 });
 
-
-cloudinary.config({
-  cloud_name: process.env.cloudinary_cloud_name,
-  api_key: process.env.cloudinary_api_key,
-  api_secret: process.env.cloudinary_api_secret
-});
-
 module.exports = app;
