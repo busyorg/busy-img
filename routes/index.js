48509d5d84c35742096a85534afbaef90b02a9ad
Nilesh Suthar <nileshsuthar@live.in>
2016-09-12 01:52:18 +0530

    Fix bug due to error 404 cloudnary urls

--- routes/index.js
+++ routes/index.js
@@ -16,42 +16,72 @@ function addCloudinaryOptions(url, options) {
   return url;
 }
 
+function getCloundaryImg(url, res, defaultName, defaultOptions) {
+  if (url) {
+    http.get(url, function (response) {
+      if (response.statusCode == 200)
+        return response.pipe(res);
+      else
+        return defaultImg(res, defaultName, defaultOptions);
+    })
+  } else {
+    return defaultImg(res, defaultName, defaultOptions);
+  }
+}
+
+function defaultImg(res, name, options) {
+  var url = cloudinary.url(name, options);
+  http.get(url, function (empty) {
+    return empty.pipe(res);
+  });
+}
+
 router.get('/@:username', function (req, res, next) {
   var username = req.params.username;
   steem.api.getAccounts([username], function (err, result) {
     try {
       var url = cloudinary.url('@' + username, { width: 128, height: 128, crop: 'fill' });
-      if (err || result.length === 0) {
-        throw new Error('image not found');
-      } else {
+      if (!(err && result.length === 0)) {
         var json_metadata = result[0].json_metadata;
         if (json_metadata.length) {
           json_metadata = JSON.parse(json_metadata);
           var profile_image = json_metadata.profile && json_metadata.profile.profile_image;
           if (profile_image) {
-            http.get(addCloudinaryOptions(profile_image, 'c_fill,w_128,h_128'), function (response) {
-              return response.pipe(res);
-            })
-          } else if (url) {
-            http.get(url, function (response) {
-              return response.pipe(res);
-            })
-          } else {
-            throw new Error('image not found');
+            var profile_image_url = addCloudinaryOptions(profile_image, 'c_fill,w_128,h_128');
+            return getCloundaryImg(profile_image_url, res, '@busy', { width: 128, height: 128, crop: 'fill' });
           }
-        } else if (url) {
-          http.get(url, function (response) {
-            return response.pipe(res);
-          })
-        } else {
-          throw new Error('image not found');
         }
+        return getCloundaryImg(url, res, '@busy', { width: 128, height: 128, crop: 'fill' });
+      } else {
+        return defaultImg(res, '@busy', { width: 128, height: 128, crop: 'fill' });
       }
     } catch (e) {
-      var url = cloudinary.url('@busy', { width: 128, height: 128, crop: 'fill' });
-      http.get(url, function (empty) {
-        return empty.pipe(res);
-      });
+      return defaultImg(res, '@busy', { width: 128, height: 128, crop: 'fill' });
+    }
+  });
+});
+
+router.get('/@:username/cover', function (req, res, next) {
+  var username = req.params.username;
+  steem.api.getAccounts([username], function (err, result) {
+    try {
+      var url = cloudinary.url('@' + username + '/cover', { width: 900, height: 250, crop: 'fill' });
+      if (!(err && result.length === 0)) {
+        var json_metadata = result[0].json_metadata;
+        if (json_metadata.length) {
+          json_metadata = JSON.parse(json_metadata);
+          var cover_image = json_metadata.profile && json_metadata.profile.cover_image;
+          if (cover_image) {
+            var cover_image_url = addCloudinaryOptions(cover_image, 'c_fill,w_900,h_250');
+            return getCloundaryImg(cover_image_url, res, '@busy/cover', { width: 900, height: 250, crop: 'fill' });
+          }
+        }
+        return getCloundaryImg(url, res, '@busy/cover', { width: 900, height: 250, crop: 'fill' });
+      } else {
+        return defaultImg(res, '@busy/cover', { width: 900, height: 250, crop: 'fill' });
+      }
+    } catch (e) {
+      return defaultImg(res, '@busy/cover', { width: 900, height: 250, crop: 'fill' });
     }
   });
 });
@@ -74,46 +104,6 @@ router.post('/@:username/cover', multipartMiddleware, function (req, res, next) 
     res.json({ url: result.url });
   }, { public_id: '@' + username + '/cover' });
   delete req.files;
-});
-
-router.get('/@:username/cover', function (req, res, next) {
-  var username = req.params.username;
-  steem.api.getAccounts([username], function (err, result) {
-    try {
-      var url = cloudinary.url('@' + username + '/cover', { width: 900, height: 250, crop: 'fill' });
-      if (err || result.length === 0) {
-        throw new Error('image not found');
-      } else {
-        var json_metadata = result[0].json_metadata;
-        if (json_metadata.length) {
-          json_metadata = JSON.parse(json_metadata);
-          var cover_image = json_metadata.profile && json_metadata.profile.cover_image;
-          if (cover_image) {
-            http.get(addCloudinaryOptions(cover_image, 'c_fill,w_900,h_250'), function (response) {
-              return response.pipe(res);
-            })
-          } else if (url) {
-            http.get(url, function (response) {
-              return response.pipe(res);
-            })
-          } else {
-            throw new Error('image not found');
-          }
-        } else if (url) {
-          http.get(url, function (response) {
-            return response.pipe(res);
-          })
-        } else {
-          throw new Error('image not found');
-        }
-      }
-    } catch (e) {
-      var url = cloudinary.url('@busy/cover', { width: 900, height: 250, crop: 'fill' });
-      http.get(url, function (empty) {
-        return empty.pipe(res);
-      });
-    }
-  });
 });
 
 
