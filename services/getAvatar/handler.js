f81961ba07e977b9407ab5c9d8864a1ba6d74a23
Nilesh Suthar <nileshsuthar@live.in>
2016-12-01 11:49:46 +0530

    Get transformed url

--- services/getAvatar/handler.js
+++ services/getAvatar/handler.js
@@ -5,29 +5,58 @@ var request = require('request');
 
 var defaultAvatar = '@steemconnect';
 
-// function defaultImg(res, name, options) {
-//   debug('show defaultImg', name);
-//   var url = cloudinary.url(name, options);
-//   res.writeHead(200, { 'Content-Type': 'image/png' });
-//   request.get(url).pipe(res);
-// }
+function showImage(url, res) {
+  return new Promise(function (resolve, reject) {
+    if (url) {
+      console.log('url', url);
+      request.get(url).on('response', function (response) {
+        console.log('response', response);
+        var contentType = response.headers['content-type'] || '';
+        if (response.statusCode == 200 && contentType.search('image') === 0) {
+          console.log('img exisit', url);
+          // res.writeHead(200, { 'Content-Type': contentType });
+          return resolve(url);
+        } else {
+          return reject(new Error('Img not found'));
+        }
+      });
+    } else {
+      return reject(new Error('invalid url not found'))
+    }
+  });
 
-// function showExternalImgOrDefault(url, res, defaultAvatar, options) {
-//   var fetchOptions = Object.assign({}, options, {
-//     type: 'fetch',
-//     sign_url: true,
-//     defaultAvatar: defaultAvatar
-//   });
+}
 
-//   var newUrl = cloudinary.url(url, fetchOptions);
+function getDefaultImg(res, name, options) {
+  console.log('getDefaultImg', name);
+  var url = cloudinary.url(name, options);
+  return url;
+  // res.writeHead(200, { 'Content-Type': 'image/png' });
+  // request.get(url).pipe(res);
+}
 
-//   return showImage(newUrl, res).catch(function (e) {
-//     return defaultImg(res, defaultAvatar, options);
-//   });
-// }
+function showExternalImgOrDefault(url, res, defaultAvatar, options) {
+  var fetchOptions = Object.assign({}, options, {
+    type: 'fetch',
+    sign_url: true,
+    defaultAvatar: defaultAvatar
+  });
+  console.log('fetchOptions', fetchOptions);
+  var newUrl = cloudinary.url(url, fetchOptions);
+  console.log('newUrl', newUrl);
+  return showImage(newUrl, res).catch(function (e) {
+    return getDefaultImg(res, defaultAvatar, options);
+  });
+}
 
 module.exports.getAvatar = (event, context, callback) => {
   const username = event.pathParameters.username;
+  const queryStringParameters = event.queryStringParameters || {};
+  const width = queryStringParameters.width || queryStringParameters.size || 128;
+  const height = queryStringParameters.height || queryStringParameters.size || 128;
+  const crop = queryStringParameters.crop || 'fill';
+  const options = { width: width, height: height, crop: crop };
+  console.log('options', options);
   request({ url: 'https://api.steemjs.com/getAccounts?names[]=' + username, json: true },
     function (error, response, body) {
       var profile_image;
@@ -38,19 +67,33 @@ module.exports.getAvatar = (event, context, callback) => {
           profile_image = json_metadata.profile && json_metadata.profile.profile_image;
         }
 
-        // if (profile_image) {
-        //   return showExternalImgOrDefault(profile_image, res, defaultAvatar, options);
-        // } else {
-        //   return defaultImg(res, defaultAvatar, options);
-        // }
-        const result = {
-          statusCode: 200,
-          body: JSON.stringify({
-            profile_image: profile_image,
-          }),
-        };
-        console.log(body) // Show the HTML for the Google homepage.
-        callback(null, result);
+        if (profile_image) {
+          console.log('here');
+          showExternalImgOrDefault(profile_image, null, defaultAvatar, options).then((composedUrl) => {
+            console.log('composedUrl', composedUrl);
+            const result = {
+              statusCode: 200,
+              body: JSON.stringify({
+                profile_image: profile_image,
+                composedUrl: composedUrl,
+              }),
+            };
+            callback(null, result);
+          });
+        } else {
+          console.log('no here');
+          getDefaultImg(null, defaultAvatar, options).then((composedUrl) => {
+            console.log('composedUrl', composedUrl);
+            const result = {
+              statusCode: 200,
+              body: JSON.stringify({
+                profile_image: profile_image,
+                composedUrl: composedUrl,
+              }),
+            };
+            callback(null, result);
+          });
+        }
       } else {
         const result = {
           statusCode: 500,
