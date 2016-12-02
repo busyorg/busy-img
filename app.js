d8aba4b5e909ddc17c150b251477969295066ea9
Nilesh Suthar <nileshsuthar@live.in>
2016-12-02 18:27:00 +0530

    letsencrypt challenge

--- app.js
+++ app.js
@@ -16,11 +16,13 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
-
+app.get('/.well-known/acme-challenge/clpPmApwieOeBkZRuGraxPGTpFaomjDwHBr2Ux5ChBI', function (req, res) {
+  res.send('clpPmApwieOeBkZRuGraxPGTpFaomjDwHBr2Ux5ChBI.jgkX-hetJtOPr-FJcfSN5VmYj5eCmmCtg_iTILuHYQg');
+})
 app.use('/', routes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -31,7 +33,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.json(['error', {
       message: err.message,
@@ -42,7 +44,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.json(['error', {
     message: err.message,
