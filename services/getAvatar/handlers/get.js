91ff7f1715eeb795fc17e9f4b6f41784c511a4cd
Nilesh Suthar <nileshsuthar@live.in>
2016-12-03 21:09:31 +0530

    Support custom defaults

--- services/getAvatar/handlers/get.js
+++ services/getAvatar/handlers/get.js
@@ -4,9 +4,9 @@ var cloudinary = require('cloudinary');
 var request = require('request');
 
 function showImage(url) {
-    return new Promise(function(resolve, reject) {
+    return new Promise(function (resolve, reject) {
         if (url) {
-            request.get(url).on('response', function(response) {
+            request.get(url).on('response', function (response) {
                 var contentType = response.headers['content-type'] || '';
                 if (response.statusCode == 200 && contentType.search('image') === 0) {
                     return resolve(url);
@@ -21,33 +21,39 @@ function showImage(url) {
 }
 
 function getDefaultImg(name, options) {
-    return cloudinary.url(name, options);
+    return options.default || cloudinary.url(name, options);
 }
 
 function showExternalImgOrDefault(url, defaultAvatar, options, cb) {
-    var fetchOptions = Object.assign({}, options, {
-        type: 'fetch',
-        sign_url: true,
-        defaultAvatar: defaultAvatar
-    });
+    var fetchOptions = Object.assign({}, options, { type: 'fetch', sign_url: true, defaultAvatar: defaultAvatar });
     var newUrl = cloudinary.url(url, fetchOptions);
-    return showImage(newUrl, cb).catch(function(e) {
+    return showImage(newUrl, cb).catch(function (e) {
         cb(null, { statusCode: 302, headers: { Location: getDefaultImg(defaultAvatar, options) } });
     }).then((url) => {
         cb(null, { statusCode: 302, headers: { Location: url } });
     });
 }
 
+function getOptions(queryStringParameters, defaultParameters) {
+    queryStringParameters = queryStringParameters || {};
+    queryStringParameters.size = queryStringParameters.size || queryStringParameters.s;
+    queryStringParameters.width = queryStringParameters.width || queryStringParameters.w || queryStringParameters.size;
+    queryStringParameters.height = queryStringParameters.height || queryStringParameters.h || queryStringParameters.size;
+    queryStringParameters.default = queryStringParameters.default || queryStringParameters.d;
+    return {
+        size: queryStringParameters.size || queryStringParameters.size,
+        width: queryStringParameters.width || queryStringParameters.width,
+        height: queryStringParameters.height || queryStringParameters.height,
+        default: queryStringParameters.default || queryStringParameters.default
+    };
+}
+
 module.exports.Avatar = (event, context, callback) => {
     const defaultAvatar = '@steemconnect';
     const username = event.pathParameters.username.match(/@?(\w+)/)[1];
-    const queryStringParameters = event.queryStringParameters || {};
-    const width = queryStringParameters.width || queryStringParameters.size || 128;
-    const height = queryStringParameters.height || queryStringParameters.size || 128;
-    const crop = queryStringParameters.crop || 'fill';
-    const options = { width: width, height: height, crop: crop };
+    const options = getOptions(event.queryStringParameters, { width: 128, height: 128, crop: 'fill' });
     request({ url: 'https://api.steemjs.com/getAccounts?names[]=' + username, json: true },
-        function(error, response, body) {
+        function (error, response, body) {
             var profile_image;
             if (body.length !== 0) {
                 var json_metadata = body[0].json_metadata;
@@ -55,7 +61,6 @@ module.exports.Avatar = (event, context, callback) => {
                     json_metadata = JSON.parse(json_metadata);
                     profile_image = json_metadata.profile && json_metadata.profile.profile_image;
                 }
-
                 if (profile_image) {
                     showExternalImgOrDefault(profile_image, defaultAvatar, options, callback);
                 } else {
@@ -70,14 +75,9 @@ module.exports.Avatar = (event, context, callback) => {
 module.exports.Cover = (event, context, callback) => {
     const defaultAvatar = '@steemconnect/cover';
     const username = event.pathParameters.username.match(/@?(\w+)/)[1];
-    const queryStringParameters = event.queryStringParameters || {};
-    const width = queryStringParameters.width || queryStringParameters.size || 850;
-    const height = queryStringParameters.height || queryStringParameters.size || 300;
-    const crop = queryStringParameters.crop || 'fill';
-    const options = { width: width, height: height, crop: crop };
-
+    const options = getOptions(event.queryStringParameters, { width: 850, height: 300, crop: 'fill' });
     request({ url: 'https://api.steemjs.com/getAccounts?names[]=' + username, json: true },
-        function(error, response, body) {
+        function (error, response, body) {
             var cover_image;
             if (body.length !== 0) {
                 var json_metadata = body[0].json_metadata;
@@ -98,7 +98,7 @@ module.exports.Cover = (event, context, callback) => {
 
 module.exports.Uploads = (event, context, callback) => {
     const username = event.pathParameters.username.match(/@?(\w+)/)[1];
-    cloudinary.api.resources_by_tag(username, function(result) {
+    cloudinary.api.resources_by_tag(username, function (result) {
         callback(null, { statusCode: 200, body: JSON.stringify(result.resources) });
     });
 }
